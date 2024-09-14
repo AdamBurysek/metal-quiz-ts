e4555dceb9430c5d9306d0be8a1c23ac9a8bc092
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2024-09-14 16:39:02 +0200

    change index.js to tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,7 @@ import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <HashRouter>
     <App />
   </HashRouter>
